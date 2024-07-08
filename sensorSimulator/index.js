725dca70a1a3b6c8d7d61e3e6992cfa14b01ee67
Serdar Akol <serdarakoll@gmail.com>
2024-07-08 18:27:24 +0200

    3 temperature sensors

--- sensorSimulator/index.js
+++ sensorSimulator/index.js
@@ -4,6 +4,7 @@ const Sensor = require('./sensor');
 console.log('Sensor Simulator started');
 
 const tempSensor = new Sensor('Temperature Sensor', 'temp-sensor-1', 0, 100, 'ws://127.0.0.1:3000');
-const humiditySensor = new Sensor('Humidity Sensor', 'humidity-sensor-1', 0, 100, 'ws://127.0.0.1:3000');
+const tempSensor2 = new Sensor('Temperature Sensor', 'temp-sensor-2', 0, 100, 'ws://127.0.0.1:3000');
+const tempSensor3 = new Sensor('Temperature Sensor', 'temp-sensor-3', 0, 100, 'ws://127.0.0.1:3000');
 
 
